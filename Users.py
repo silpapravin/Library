2d561d53f8e617cf5bb9b000332a2e7aece5f967
Bruna Weber <brunadaniweber@hotmail.com>
2021-06-16 11:41:01 +0100

    Creation of Users Dictionary: new file:   Users.py

---
+++ Users.py
@@ -0,0 +1,7 @@
+user1 = {"IDNumber": "01", "Name":"Ivan", "Surname": "Cooper", "BorrewedBooks":[]}
+user2 = {"IDNumber": "02", "Name":"Fiona", "Surname": "Murphy", "BorrewedBooks":[1,2]}
+user3 = {"IDNumber": "03", "Name":"Aoife", "Surname": "Walsh", "BorrewedBooks":[4]}
+user4 = {"IDNumber": "04", "Name":"Brendan", "Surname": "O'Brien", "BorrewedBooks":[]}
+user5 = {"IDNumber": "05", "Name":"Peter", "Surname": "O'Neill", "BorrewedBooks":[]}
+user6 = {"IDNumber": "06", "Name":"Declan", "Surname": "Kennedy", "BorrewedBooks":[6,8,9]}
+user7 = {"IDNumber": "07", "Name":"Rylee", "Surname": "Lynch", "BorrewedBooks":[3]}
