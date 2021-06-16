46b52c6aa57589628078b53042f5ab03f707c434
Bruna Weber <brunadaniweber@hotmail.com>
2021-06-16 15:37:13 +0100

    Change the Id of Books that are Borrewed

--- Users.py
+++ Users.py
@@ -1,13 +1,13 @@
 userList = []
 
 
-userList.append({"IDNumber": "01", "isLibrarian": False, "Username": "IvanCooper", "Password": "1237", "Name":"Ivan", "Surname": "Cooper", "BorrewedBooks":[]})
-userList.append({"IDNumber": "02", "isLibrarian": False, "Username": "FionaMurphy", "Password": "2450", "Name":"Fiona", "Surname": "Murphy", "BorrewedBooks":[1,2]})
-userList.append({"IDNumber": "03", "isLibrarian": False, "Username": "AoifeWalsh", "Password": "2753", "Name":"Aoife", "Surname": "Walsh", "BorrewedBooks":[4]})
+userList.append({"IDNumber": "01", "isLibrarian": False, "Username": "IvanCooper", "Password": "1237", "Name":"Ivan", "Surname": "Cooper", "BorrewedBooks":[7006, 1200]})
+userList.append({"IDNumber": "02", "isLibrarian": False, "Username": "FionaMurphy", "Password": "2450", "Name":"Fiona", "Surname": "Murphy", "BorrewedBooks":[]})
+userList.append({"IDNumber": "03", "isLibrarian": False, "Username": "AoifeWalsh", "Password": "2753", "Name":"Aoife", "Surname": "Walsh", "BorrewedBooks":[3005]})
 userList.append({"IDNumber": "04", "isLibrarian": False, "Username": "BrendanO'Brien", "Password": "2745", "Name":"Brendan", "Surname": "O'Brien", "BorrewedBooks":[]})
 userList.append({"IDNumber": "05", "isLibrarian": False, "Username": "PeterO'Neill", "Password": "2457", "Name":"Peter", "Surname": "O'Neill", "BorrewedBooks":[]})
-userList.append({"IDNumber": "06", "isLibrarian": False, "Username": "DeclanKennedy", "Password": "2821", "Name":"Declan", "Surname": "Kennedy", "BorrewedBooks":[6,8,9]})
-userList.append({"IDNumber": "07", "isLibrarian": True, "Username": "RyleeLynch", "Password": "0415", "Name":"Rylee", "Surname": "Lynch", "BorrewedBooks":[3]})
+userList.append({"IDNumber": "06", "isLibrarian": False, "Username": "DeclanKennedy", "Password": "2821", "Name":"Declan", "Surname": "Kennedy", "BorrewedBooks":[]})
+userList.append({"IDNumber": "07", "isLibrarian": True, "Username": "RyleeLynch", "Password": "0415", "Name":"Rylee", "Surname": "Lynch", "BorrewedBooks":[1234, 5664]})
 
 def DisplayUsers():
     print("Library Users:")
