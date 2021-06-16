10fb1ec38ad55acc032c8b010c285489e0d4a408
Megan Davitt <megandavitt1998@gmail.com>
2021-06-16 12:09:55 +0100

    Booklist and Search Function

---
+++ booklist.py
@@ -0,0 +1,27 @@
+booklist = []
+
+book = {"title": "Where The Wild Things Are", "author": "Maurice Sendak", "onloan": False}
+booklist.append(book)
+booklist.append({"title": "The Very Hungry Caterpillar", "author": "Eric Carle", "onloan": False})
+booklist.append({"title": "The Giving Tree", "author": "Shel Silverstein", "onloan": False})
+booklist.append({"title": "Green Eggs and Ham", "author": "Dr. Suess", "onloan": False})
+booklist.append({"title": "Goodnight Moon", "author": "Margaret Wise Brown", "onloan": False})
+booklist.append({"title": "Charlotte's Web", "author": "E.B. White", "onloan": False})
+booklist.append({"title": "The Lion, The Witch and The Wardrobe", "author": "C.S. Lewis", "onloan": False})
+booklist.append({"title": "She: A History of Adventure", "author": "H. Rider Haggard", "onloan": False})
+booklist.append({"title": "Charlotte's Web", "author": "E.B. White", "onloan": False})
+booklist.append({"title": "Alice's Adventures in Wonderland", "author": "Lewis Carroll", "onloan": False})
+booklist.append({"title": "The Hobbit", "author": "J.R.R. Tolkien", "onloan": False})
+booklist.append({"title": "And Then There Were None", "author": "Agatha Christie", "onloan": False})
+booklist.append({"title": "Harry Potter", "author": "J.K. Rowling", "onloan": False})
+booklist.append({"title": "The Little Prince", "author": " Antoine de Saint-Exupéry", "onloan": False})
+booklist.append({"title": "The Lord of The Rings", "author": " J.R.R. Tolkien", "onloan": False})
+booklist.append({"title": "A Tale of Two Cities", "author": "Charles Dickens", "onloan": False})
+booklist.append({"title": "The Little Prince", "author": " Antoine de Saint-Exupéry", "onloan": False})
+booklist.append({"title": "Everything I Know About Love", "author": "Dolly Adlerton", "onloan": False})
+searchTerm = "Are"
+
+for book in booklist:
+    print(f"{book['title'].ljust(30)} {book['author'].ljust(20)} {str(book['onloan'])}")
+    if(book['title'].find(searchTerm) != -1):
+        print(f"    Contains '{searchTerm}'")
