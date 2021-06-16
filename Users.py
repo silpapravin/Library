c33ccc670bef6eaa522b86bd3c9a73a5d0cb602a
Bruna Weber <brunadaniweber@hotmail.com>
2021-06-16 12:21:19 +0100

    Append Users to list modified:   Users.py

--- Users.py
+++ Users.py
@@ -1,8 +1,13 @@
-UsersList = []
-user1 = {"IDNumber": "01", "Name":"Ivan", "Surname": "Cooper", "BorrewedBooks":[]}
-user2 = {"IDNumber": "02", "Name":"Fiona", "Surname": "Murphy", "BorrewedBooks":[1,2]}
-user3 = {"IDNumber": "03", "Name":"Aoife", "Surname": "Walsh", "BorrewedBooks":[4]}
-user4 = {"IDNumber": "04", "Name":"Brendan", "Surname": "O'Brien", "BorrewedBooks":[]}
-user5 = {"IDNumber": "05", "Name":"Peter", "Surname": "O'Neill", "BorrewedBooks":[]}
-user6 = {"IDNumber": "06", "Name":"Declan", "Surname": "Kennedy", "BorrewedBooks":[6,8,9]}
-user7 = {"IDNumber": "07", "Name":"Rylee", "Surname": "Lynch", "BorrewedBooks":[3]}
+userList = []
+
+userList.append(user)
+
+userList.append({"IDNumber": "01", "Password": "1237", "Name":"Ivan", "Surname": "Cooper", "BorrewedBooks":[]})
+userList.append({"IDNumber": "02", "Password": "2450", "Name":"Fiona", "Surname": "Murphy", "BorrewedBooks":[1,2]})
+userList.append({"IDNumber": "03", "Password": "2753", "Name":"Aoife", "Surname": "Walsh", "BorrewedBooks":[4]})
+userList.append({"IDNumber": "04", "Password": "2745", "Name":"Brendan", "Surname": "O'Brien", "BorrewedBooks":[]})
+userList.append({"IDNumber": "05", "Password": "2457", "Name":"Peter", "Surname": "O'Neill", "BorrewedBooks":[]})
+userList.append({"IDNumber": "06", "Password": "2821", "Name":"Declan", "Surname": "Kennedy", "BorrewedBooks":[6,8,9]})
+userList.append({"IDNumber": "07", "Password": "0415", "Name":"Rylee", "Surname": "Lynch", "BorrewedBooks":[3]})
+
+print(userList)
