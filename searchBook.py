6930b120bf3ffbf666d4e91ddaecb5eaa5353010
SumayaAlmeida <sumaya.almeida@gmail.com>
2021-06-17 10:17:54 +0100

    'naming convention'

--- searchBook.py
+++ searchBook.py
@@ -3,13 +3,13 @@ import booklist
 
 def general_search():
     
-    searchTerm = input("What would you like to search?\n").capitalize()
+    search_term = input("What would you like to search?\n").capitalize()
     search_list = 0
     print("================================================")
     print("Results:\n")
 
     for book in booklist:
-        if(book['title'].find(searchTerm) != -1):
+        if(book['title'].find(search_term) != -1):
             print(f"{book['title'].ljust(30)} {book['author'].ljust(20)} {str(book['onloan'])}")
             search_list += 1
         else:
