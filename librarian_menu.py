d68749d2f0a24031db82c544d5310c571339331d
SumayaAlmeida <sumaya.almeida@gmail.com>
2021-06-17 16:38:17 +0100

    'last commit of thursday'

--- librarian_menu.py
+++ librarian_menu.py
@@ -2,6 +2,7 @@ import os
 import search_book
 import user_file
 import see_list_of_books
+import login
 
 user_list = user_file.user_list
 book_list = see_list_of_books.book_list
@@ -11,7 +12,7 @@ def clear_screen():
 
 def display_librarian_menu():
     clear_screen()
-    print(f"Welcome again #####name, Menu options:")
+    print(f"Welcome again dear Librarian, Menu options:")
     print("1. Search a book")
     print("2. See What books are borrowed")
     print("3. See See What books are overdue")
@@ -27,23 +28,29 @@ def display_librarian_menu():
             print("You selected Option one!\n")
             print("===============================")
             search_book.general_search()
-            input("Return to continue...")
+            display_librarian_menu()
         elif(choice == "2"):
             print("You selected Option two!\n")
+            user_file.all_borrowed_book()
             input("Return to continue...")
-
+            display_librarian_menu()
         elif (choice == "3"):
             print("You selected Option three!\n")
             input("Return to continue...")
+            display_librarian_menu()
         elif(choice == "4"):
             print("You selected Option four!\n")
+            user_file.display_users()
             input("Return to continue...")
-        elif(choice != "5"):
+            display_librarian_menu()
+        elif(choice == "5"):
             print("You selected Option five!\n")
             see_list_of_books.printbook_list()
             input("Return to continue...")
+            display_librarian_menu()
         elif(choice != "6"):
             print("Please return to menu and select a digit from 1 to 6")
             ("Return to continue...")
+            display_librarian_menu()
 
-        print("Bye!")
+    print("Bye!")
