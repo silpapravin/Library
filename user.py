60941d8e0f668cd19ca329970e0abd96f1c270a6
Esther2098 <64903210+Esther2098@users.noreply.github.com>
2021-06-24 15:39:22 +0100

    Update user.py

--- user.py
+++ user.py
@@ -4,9 +4,9 @@
 class User:
     #constructor
     def __init(self, username, password, ID, name, borrowing, borrowed):
-        self.username=username
-        self.password=password
-        self.ID=ID
-        self.name=name
-        self.borrowing=borrowing #need to come back to boolean values in constructor and lists
-        self.borrowed=borrowed
+        self._username=username
+        self._password=password
+        self._ID=ID
+        self._name=name
+        self._borrowing=borrowing #need to come back to boolean values in constructor and lists
+        self._borrowed=borrowed
