ead6b6a2fb79d37ba9a4c7fb9e8e4f27d3955e7a
SumayaAlmeida <sumaya.almeida@gmail.com>
2021-06-24 14:23:20 +0100

    'book'

---
+++ book.py
@@ -0,0 +1,10 @@
+
+class Book: 
+
+    def __init__(self, title, author, book_id, on_loan, date_loaned):
+        self.title = title
+        self.author = author 
+        self.book_id = book_id
+        self.on_loan = on_loan
+        self.date_loaned = date_loaned
+        
