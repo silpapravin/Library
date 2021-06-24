4672c4e276047180d513707d75695031e3bb548d
Megan Davitt <megandavitt1998@gmail.com>
2021-06-24 15:41:49 +0100

    Book Class with Parameters

--- book.py
+++ book.py
@@ -2,9 +2,42 @@
 class Book: 
 
     def __init__(self, title, author, book_id, on_loan, date_loaned):
-        self.title = title
-        self.author = author 
-        self.book_id = book_id
-        self.on_loan = on_loan
-        self.date_loaned = date_loaned
-        
+        self._title = title
+        self._author = author 
+        self._book_id = book_id
+        self._on_loan = on_loan
+        self._date_loaned = date_loaned
+
+#property
+    def title(self):
+        return self._title
+
+#title.setter
+    def title(self, new_title):
+        self._title = new_title
+
+    def author(self):
+        return self._author
+            
+    def author(self, new_author):
+        self._author = new_author
+    
+    def book_id(self):
+        return self._book_id
+
+    def book_id(self, new_book_id):
+        self._book_id = new_book_id
+
+    def on_loan(self):
+        return self._on_loan
+    
+    def on_loan(self, new_on_loan):
+        self._on_loan = new_on_loan
+    
+    def date_loaned(self):
+        return self.date_loaned
+
+    def on_loan(self, new_date_loaned):
+        self._date_loaned = new_date_loaned
+
+
