7e9f85f4d07d8f2e8f47fd58cdfdb59dea834c7a
Esther2098 <64903210+Esther2098@users.noreply.github.com>
2021-06-25 12:24:23 +0100

    Update user.py

--- user.py
+++ user.py
@@ -10,46 +10,59 @@ class User:
         self.__is_librarian=is_librarian
 
     #getters and setters
-
+    @property
     def get_username(self):
         return self.__username
-
-    def set_password(self, new_username):
+    
+    @username.setter 
+    def set_username(self, new_username):
         self.__username = new_username
-
+        
+    @property
     def get_password(self):
         return self.__password
     
+    @password.setter 
     def set_password(self, new_password):
         self.__password = new_password
-
+     
+    @property
     def get_ID(self):
         return self.__ID
     
+    @ID.setter 
     def set_ID(self, new_ID):
         self.__ID = new_ID
-
+        
+    @property
     def get_name(self):
         return self.__name
     
+    @name.setter 
     def set_name(self, new_name):
         self.__name = new_name
-    
+        
+    @property
     def get_borrowed(self):
         return self.__borrowed
     
+    @borrowed.setter 
     def set_borrowed(self, new_borrowed):
         self.__borrowed = new_borrowed
-
+        
+    @property
     def get_borrowing(self):
         return self.__borrowing
     
+    @borrowing.setter 
     def set_borrowing(self, new_borrowing):
         self.__borrowing = new_borrowing
-    
+        
+    @property
     def get_is_librarian(self):
         return self.__is_librarian
     
+    @is_librarian.setter 
     def set_is_librarian(self,new_is_librarian):
         self.__is_librarian = new_is_librarian
 
