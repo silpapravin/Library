1256b6a06ec701c2e6c146cac028a6c2a36da2bc
Esther2098 <64903210+Esther2098@users.noreply.github.com>
2021-06-25 12:03:42 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,19 +1,61 @@
-
-
-
 class User:
     #constructor
-    def __init__(self, username, password, ID, name, surname, borrowing, borrowed, is_librarian):
-        borrowing = []
-        borrowed = []
-        self._username=username
-        self._password=password
-        self._ID=ID
-        self._name=name
-        self._surname = surname
-        self._borrowing=borrowing #need to come back to boolean values in constructor and lists
-        self._borrowed=borrowed
-        self._is_librarian = is_librarian
+    def __init(self, username, password, ID, name, borrowing, borrowed, is_librarian):
+        self.__username=username
+        self.__password=password
+        self.__ID=ID
+        self.__name=name
+        self.__borrowing=borrowing #need to come back to boolean values in constructor and lists
+        self.__borrowed=borrowed
+        self.__is_librarian=is_librarian
+
+    #getters and setters
+
+    def get_username(self):
+        return self.__username
+
+    def set_password(self, new_username):
+        self.__username = new_username
+
+    def get_password(self):
+        return self.__password
+    
+    def set_password(self, new_password):
+        self.__password = new_password
+
+    def get_ID(self):
+        return self.__ID
+    
+    def set_ID(self, new_ID):
+        self.__ID = new_ID
+
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, new_name):
+        self.__name = new_name
+    
+    def get_borrowed(self):
+        return self.__borrowed
+    
+    def set_borrowed(self, new_borrowed):
+        self.__borrowed = new_borrowed
+
+    def get_borrowing(self):
+        return self.__borrowing
+    
+    def set_borrowing(self, new_borrowing):
+        self.__borrowing = new_borrowing
+    
+    def get_is_librarian(self):
+        return self.__is_librarian
+    
+    def set_is_librarian(self,new_is_librarian):
+        self.__is_librarian = new_is_librarian
+
+
+    
+
 
     def description(self):
-        return f"User:       {str(self._ID).ljust(4)} {self._name.ljust(20)} {self._surname.ljust(25)}"
+        return f"User:       {str(self.__username).ljust(10)} {self.__password.ljust(20)} {self.__ID.ljust(10)} {self.__name.ljust(20)} {self.__borrowing.ljust()} {self.__borrowed.ljust()} {self.__is_librarian.ljust()}"
