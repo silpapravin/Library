4133dd2a69389ae9c5ee50b4efddef8c7fa0ac30
Megan Davitt <megandavitt1998@gmail.com>
2021-06-25 10:28:42 +0100

    Book Class with Getter and Setters

--- book.py
+++ book.py
@@ -8,35 +8,43 @@ class Book:
         self._on_loan = on_loan
         self._date_loaned = date_loaned
 
-#property
+    @property
     def title(self):
         return self._title
 
-#title.setter
+    @title.setter
     def title(self, new_title):
         self._title = new_title
 
+    @property
     def author(self):
         return self._author
-            
+
+    @author.setter        
     def author(self, new_author):
         self._author = new_author
     
+    @property
     def book_id(self):
         return self._book_id
 
+    @book_id.setter
     def book_id(self, new_book_id):
         self._book_id = new_book_id
 
+    @property
     def on_loan(self):
         return self._on_loan
     
+    @on_loan.setter
     def on_loan(self, new_on_loan):
         self._on_loan = new_on_loan
     
+    @property
     def date_loaned(self):
         return self.date_loaned
 
+    @date_loaned.setter
     def on_loan(self, new_date_loaned):
         self._date_loaned = new_date_loaned
 
