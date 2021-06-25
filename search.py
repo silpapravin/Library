8bfe4c933226467eb8b8c6ed1b6383c1c6a7b512
SumayaAlmeida <sumaya.almeida@gmail.com>
2021-06-25 11:51:07 +0100

    'search function working'

--- search.py
+++ search.py
@@ -1,32 +1,34 @@
 from datastore import book_list
 from book import Book
+from menu import Menu
 
 class Search:
 
-       
+    local_book_list = book_list
+
     def general_search(book_list):
-        
+            
             print(book_list)
             search_term = input("What would you like to search?\n").lower()
-            search_list = 0
+            search_list = []
             print("================================================")
-            print("Results:\n")
+            
 
             for book in book_list:
-                book_title = str(getattr(book,'title')).lower()
-                if(book_title.find(search_term) != -1):
-                    print(book.description())
-                    #print(f"{str(getattr(book,'title')).ljust(30)} {str(getattr(book,'author')).ljust(20)} {str(getattr(book,'on_loan'))}")
-                    search_list += 1
+                if (book.title.lower().count(search_term)) or (book.author.lower().count(search_term)):
+                    search_list.append(book)
+                         
                 else:
                     continue
-
-            if search_list == 0:
+                
+                print(f"Results: {len(search_list)}\n")
+                for book in search_list:
+                    print(book.description())
+        
+            if len(search_list) == 0:
                 print(f'There are no books with this word')
 
             input("Return to continue...")
-
-    general_search(book_list) 
 
 
     def print_user_list ():
@@ -40,3 +42,13 @@ class Search:
 
     def display_users():
         print()
+
+    general_search(local_book_list) 
+    #print(local_book_list)
+
+    #checking access to attibutes
+    def checking_attributes():
+        for book in book_list:
+            print(str(book.title))
+
+    #checking_attributes()
